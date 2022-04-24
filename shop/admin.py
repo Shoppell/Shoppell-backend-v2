badaddb687821a74ee51c66627b8208925196c50
navid falah <navid.falah7@gmail.com>
2022-04-24 19:03:39 +0430

    product image admin added

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Product, Shop, Category
+from .models import Product, Shop, Category, ProductImage
 
+admin.site.register(ProductImage)
 admin.site.register(Category)
 admin.site.register(Shop)
 admin.site.register(Product)
