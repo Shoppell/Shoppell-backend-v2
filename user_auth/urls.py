cc941f55e5fe51fd6d0f9338f35b68814801bb86
navid falah <navid.falah7@gmail.com>
2022-04-25 00:57:00 +0430

    user rud added

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -4,5 +4,5 @@ from user_auth import api
 urlpatterns = [
     path('register', api.RegisterApi.as_view()),
     path('verify', api.LoginApi.as_view()),
-    path('rud', api.UserRUD.as_view()),
+    path('rud/<int:pk>', api.UserRUD.as_view()),
 ]
