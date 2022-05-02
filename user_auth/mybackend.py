fbe8ec70937a9ad0f7e8793152aa53508ed1b646
navid falah <navid.falah7@gmail.com>
2022-05-02 23:33:29 +0430

    backend auth changed

--- user_auth/mybackend.py
+++ user_auth/mybackend.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.backends import ModelBackend
+from requests import Response
 from .models import User
 
 
 class MobileBackend(ModelBackend):
 
     def authenticate(self, request, username=None, password=None, **kwargs):
-        mobile = kwargs['mobile']
-        try:
-            user = User.objects.get(mobile=mobile)
-        except User.DoesNotExist:
-            pass
-
+        if 'mobile' in kwargs:
+            mobile = kwargs['mobile']
+            try:
+                user = User.objects.get(mobile=mobile)
+            except User.DoesNotExist:
+                pass
